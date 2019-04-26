444af6826218ddb61084a7fc6fa29fbc932292fd
Jeremias Pino <jerepino@gmail.com>
2019-04-26 17:16:18 -0300

    Modelo pendulo corregido y funcionando

--- Inferencia_difusa/modelo_carro_pendulo.py
+++ Inferencia_difusa/modelo_carro_pendulo.py
@@ -1,35 +1,28 @@
 import matplotlib.pyplot as plt
-import math
-import numpy
+from math import sin, cos, pow, pi, degrees
 
 
-def modelo(tita, tita_v, tita_a, dt, F = 0.0):
+def modelo(tita, dt):
     """
-    :param tita: angulo del pendulo
-    :param tita_v: velocidad angular del pendulo
+    :param tita: [angulo, velocidad, aceleracion] del pendulo
     :param dt: delta del tiempo
-    :param F: Entrada de control. En este caso Fuerza
-    :return : [posicion angular , velocidad angular, aceleracion angular] [rad]
+    :return tita_n: [posicion angular , velocidad angular, aceleracion angular] [rad]
     """
-    g = 9.81
-    M = 2
-    m = 0.5
-    l = 0.5
+    tita_n = tita
+    numerador = g * sin(tita[0]) + cos(tita[0]) * (- F - m * l * pow(tita[1], 2) * sin(tita[0])) / (M + m)
+    denominador = l * (4 / 3 - m * pow(cos(tita[0]), 2) / (M + m))
+    tita_n[2] = numerador / denominador
+    tita_n[1] = tita[1] + tita[2] * dt
+    tita_n[0] = tita[0] + tita[1] * dt + tita[2] * pow(dt, 2) / 2
 
-    tita_n = tita + tita_v * dt + (tita_a * dt ** 2) / 2
-    tita_v_n = tita + tita_a * dt
-    tita_a_n = (g * math.sin(tita_n) + math.cos(tita_n) *
-                ((-F - m * l * tita_v_n ** 2 * math.sin(tita_n)) / (M + m))) \
-               / l * (4 / 3 - (m * math.pow(math.cos(tita_n), 2))
-                      / (M + m))
-    return [tita_n, tita_v_n, tita_a_n]
+    return tita_n
 
 
 if __name__ == '__main__':
     # Datos
     g = 9.81
     M = 2
-    m = 0.5
+    m = 0.2
     l = 0.5
     F = 0
     # Vectores para plotear
@@ -38,60 +31,53 @@ if __name__ == '__main__':
     vector_tita_a = []
     # Estado inicial
 
-    tita_0 = -0.01 #-math.pi/10
+    tita_0 = 0.01
     tita_v_0 = 0
-    tita_a_0 = (g * math.sin(tita_0) + math.cos(tita_0) *
-              ((-F - m * l * tita_v_0 ** 2 * math.sin(tita_0)) / (M + m))) \
-             / l * (4/3 - (m * math.pow(math.cos(tita_0), 2))
-                    / (M + m))
+    tita_a_0 = 0
+
+
     # definicion del tiempo
     t = 0 #tiempo inicial
-    dt = 0.1 #paso
+    dt = 0.001 #paso
+    tiempo = []
+
     #armo el estado inicial
     estado_act = [tita_0, tita_v_0, tita_a_0]
-    vector_tita.append(estado_act[0])
-    vector_tita_v.append(estado_act[1])
-    vector_tita_a.append(estado_act[0])
-    while t <= 3.5:
-        estado_act = modelo(estado_act[0], estado_act[1], estado_act[2], dt)
+
+    while t < 2000:
         vector_tita.append(estado_act[0])
         vector_tita_v.append(estado_act[1])
-        vector_tita_a.append(estado_act[0])
+        vector_tita_a.append(estado_act[2])
+
+        estado_act = modelo(estado_act, dt)
+
+        tiempo.append(t)
         t += dt
 
-    # Paso tita de radianas a grados
-    for i, valor in enumerate(vector_tita):
-        vector_tita[i] = math.degrees(valor)
-
     #Ploteo los resultados
-    plt.figure(1)
 
-    axes = plt.subplot(311)
+    fig, (a1, a2, a3) = plt.subplots(3,1)
 
-    axes.set_ylabel('tita [°]')
+    a1.set_ylabel('tita [rad]')
 
-    axes.set_title('Posicion, velocidad y aceleracion  angular pendulo')
+    a1.set_title('Posicion, velocidad y aceleracion  angular pendulo')
 
-    plt.plot([n for n in numpy.arange(0, 3.5 + dt, dt)], vector_tita, 'b' , label="Posicion Angular")
+    a1.plot(tiempo, vector_tita, 'b', label="Posicion Angular")
 
-    plt.legend()
+    a1.legend()
 
-    axes_ = plt.subplot(312)
+    a2.set_ylabel('velocidad [rad/s]')
 
-    axes_.set_ylabel('tita_v [radianes/s]')
+    a2.plot(tiempo, vector_tita_v, 'g', label="Velocidad Angular")
 
-    plt.plot([n for n in numpy.arange(0, 3.5 + dt, dt)], vector_tita_v, 'g', label="Velocidad Angular")
+    a2.legend()
 
-    plt.legend()
+    a3.set_xlabel('Tiempo')
 
-    axes_ = plt.subplot(313)
+    a3.set_ylabel('Aceleracion [rad/s²]')
 
-    axes_.set_xlabel('Tiempo')
+    a3.plot(tiempo, vector_tita_a, 'r', label="Aceleracion Angular")
 
-    axes_.set_ylabel('tita_a [radianes/s²]')
-
-    plt.plot([n for n in numpy.arange(0, 3.5 + dt, dt)], vector_tita_a, 'r', label="Aceleracion Angular")
-
-    plt.legend()
+    a3.legend()
 
     plt.show()
